5ad3baaa65c35e7608300a37b59c13f0e6d92ebc
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2024-09-25 12:33:15 -0600

    Agregar archivos de proyecto.

---
+++ PokemonPWA/wwwroot/serviceworker.js
@@ -0,0 +1,36 @@
+﻿// service-worker.js
+
+
+let urls = [
+    "/index",
+    "/datos",
+    "/estilos.css",
+    "/imgs/icono.png",
+    "/imgs/icono-128.png",
+    "/imgs/icono-512.png",
+    "/imgs/pokemon.png"
+];
+let cacheName = "pokemonCachev1";
+async function precache() {
+    let cache = await caches.open(cacheName);
+    await cache.addAll(urls);
+}
+//PRecache
+self.addEventListener("install", function (e) {
+    e.waitUntil(precache());
+});
+
+
+self.addEventListener('fetch', event => {
+    event.waitUntil(getFromCache(event.request));
+});
+
+async function getFromCache(url) {
+    let cache = await caches.open(cacheName);
+    let response = await cache.match(url);
+    if (response) {
+        return response;
+    } else {
+        return await fetch(url);
+    }
+}
